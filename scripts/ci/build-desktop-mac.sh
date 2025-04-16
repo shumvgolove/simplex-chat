138eb854fc6ec588a39af09c3eec05abed2bfee6
shum <shum@liber.li>
2025-04-16 14:27:39 +0000

    fix

--- scripts/ci/build-desktop-mac.sh
+++ scripts/ci/build-desktop-mac.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-trap "rm apps/multiplatform/local.properties 2> /dev/null || true; rm local.properties 2> /dev/null || true; rm /tmp/simplex.keychain" EXIT
 scripts/desktop/build-lib-mac.sh
 cd apps/multiplatform
 ./gradlew packageDmg
