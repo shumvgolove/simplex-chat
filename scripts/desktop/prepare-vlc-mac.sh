a221ce8f651f620b7298e29b9a0e87eca6afa458
shum <shum@liber.li>
2025-05-15 06:25:58 +0000

    ci: switch to vlc .3 build

--- scripts/desktop/prepare-vlc-mac.sh
+++ scripts/desktop/prepare-vlc-mac.sh
@@ -9,7 +9,7 @@ if [ "$ARCH" == "arm64" ]; then
 else
     vlc_arch=intel64
 fi
-vlc_version='3.0.21-custom.2'
+vlc_version='3.0.21-custom.3'
 
 function readlink() {
   echo "$(cd "$(dirname "$1")"; pwd -P)"
